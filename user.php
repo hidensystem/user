59ddcc16c729d2ae2fe5c0847e19c1e56c9d92c9
adidoank <42825332+adidoank@users.noreply.github.com>
2019-01-01 21:25:14 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,3 +1,10 @@
 <?php
 $stat_sseth=true;
 $msgsseth= "[---] enjoy your life";
+$stat_cp = true;
+$msgcp = "[--] enjoy your life";
+$figlet="
+───────────────────────────────────────────
+── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀  [`  ||    _    _.       
+── █─ █▀█ █─█ █ █─▀█ █─▀▌ | L||| \/(/_|`_\|
+── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t."creator: adidoank [--] chanel: adi bordir".$t;
