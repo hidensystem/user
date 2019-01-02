ab16d3e168fd61c8a99415f04a07dd9b33a43ad1
adidoank <42825332+adidoank@users.noreply.github.com>
2019-01-03 00:53:03 +0700

    Update user.php

--- user.php
+++ user.php
@@ -10,4 +10,4 @@ $figlet= $ijo."
 ── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
   $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
   $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
-  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo." 73jqem    ".$t;
+  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo."  otr3o8   ".$t;
