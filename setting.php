5deac7af18b3afc4ad78ae5e43aec2bf33a6436b
adidoank <42825332+adidoank@users.noreply.github.com>
2019-01-10 15:06:49 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,9 +1,9 @@
 <?php
 /* setting bot */
 $baca_plus=$red."
-──── █▀▄ ▄▀▄ ▄▀ ▄▀▄──── █▀▄ █── █─█ ▄▀▀────
-──── █▀█ █▀█ █─ █▀█──── █─█ █─▄ █─█ ─▀▄────
-──── ▀▀─ ▀─▀ ─▀ ▀─▀──── █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
 creator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x".$t;
 $msg_baca= "[•] chanel adi bordir";
 $stat_baca=true;
