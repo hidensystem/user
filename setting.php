b7e73b27e7f6beb82a7edda8e41cef77aad9ba8a
adidoank <42825332+adidoank@users.noreply.github.com>
2019-01-13 07:32:02 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -7,6 +7,6 @@ $baca_plus=$red."
 creator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x$putih
 chanel :$kuning sungging$putih || code invite:$ijo F3NW34owm4".$t;
 $msg_baca= "[•] official chanel adi bordir";
-$stat_baca=true;
+$stat_baca=false;
  
 ?>
