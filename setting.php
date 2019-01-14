63cdda97f9d9b6ae87f9163dd25c107e9682c4a1
adidoank <42825332+adidoank@users.noreply.github.com>
2019-01-15 01:26:24 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -6,7 +6,7 @@ $baca_plus=$red."
 ──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
 creator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x$putih
 chanel :$kuning sungging$putih || code invite:$ijo F3NW34owm4".$t;
-$msg_baca= "[•] official chanel adi bordir";
+$msg_baca= "[•] official chanel adi bordir".$t."[•] info bagi yang mengalami kesalahan saat login silahkan download ulang skrip".$t."[•] kesalahan terdapat pada versi os lolipop";
 $stat_baca=true;
  
 ?>
