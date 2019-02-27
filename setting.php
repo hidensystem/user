815874758c415335b67959fa2c47729bb78113ad
adidoank <42825332+adidoank@users.noreply.github.com>
2019-02-27 15:08:26 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -114,7 +114,7 @@ $epi=$kn."
  █▀▀──── █─█──── █──── █───── █▀█──── ─▀▄──── █▀▄
  ▀▀▀──── █▀───── ▀──── ─▀──── ▀─▀──── ▀▀───── ▀─▀
  ".$pth."[ ".$tr."v 1.0".$pth." ]==[ ".$tr."creator:".$ijo." adidoank".$pth." ]==[".$tr." ref: ".$ijo."TVFG9W".$pth." ]".$t;
-$msg_epi=$pth."[support:".$tr." khairil".$pth."][Kode: ".$ijo."ZAVGFX]".$t;
+$msg_epi=$ijo."[•] update skrip https://github.com/adidoank/epicash".$t.$pth."[support:".$tr." khairil".$pth."][Kode: ".$ijo."ZAVGFX]".$t;
 $stat_epi1=true;
 
 $cash= $br."
